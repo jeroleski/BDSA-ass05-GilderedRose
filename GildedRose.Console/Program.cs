8205ba62b6330aeceba0d9e91b0c83e8c3263103
jeroleski <74965501+jeroleski@users.noreply.github.com>
2021-10-18 13:13:38 +0200

    wrote comments for all cases in if-statement

--- GildedRose.Console/Program.cs
+++ GildedRose.Console/Program.cs
@@ -37,37 +37,38 @@ namespace GildedRose.Console
         public void UpdateQuality()
         {
             for (var i = 0; i < Items.Count; i++)
-            {
+            {   //NORMAL items, which decreases in value
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
+                {   //only if the quality is positive
                     if (Items[i].Quality > 0)
-                    {
+                    {   //Sulfuras never decreases
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
+                        {   //Standard degrades
                             Items[i].Quality = Items[i].Quality - 1;
                         }
                     }
                 }
                 else
-                {
+                {   //A quality can never extend 50
                     if (Items[i].Quality < 50)
-                    {
+                    {   //Standart Special items quality increaese
                         Items[i].Quality = Items[i].Quality + 1;
-
+                        
+                        //Backstage passes has extra quality increases
                         if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
+                        {   //If the concert is within 10 days
                             if (Items[i].SellIn < 11)
                             {
                                 if (Items[i].Quality < 50)
-                                {
+                                {   //The quality rises by 2 (the one before and this one)
                                     Items[i].Quality = Items[i].Quality + 1;
                                 }
                             }
-
+                            //If the concert is within 5 days
                             if (Items[i].SellIn < 6)
                             {
                                 if (Items[i].Quality < 50)
-                                {
+                                {   //The quality rises by 3 (two before and this one)
                                     Items[i].Quality = Items[i].Quality + 1;
                                 }
                             }
@@ -75,34 +76,37 @@ namespace GildedRose.Console
                     }
                 }
 
+                //ALL items selling days will decrease
+                //exept Sulfuras never has to be sold
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
-
+                
+                //If a experation date is expired
                 if (Items[i].SellIn < 0)
-                {
+                {   //And it is not Aged Brie
                     if (Items[i].Name != "Aged Brie")
-                    {
+                    {   //And it is not Backstage Passes
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
+                        {   //And the quality is still positive
                             if (Items[i].Quality > 0)
-                            {
+                            {   //And the item is not Sulfuras
                                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
+                                {   //The quality decreases an extra amount
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
                             }
-                        }
+                        }   //For Backstage Passes
                         else
-                        {
+                        {   //The quality will drop to 0
                             Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
-                    }
+                    }   //For Aged Brie
                     else
                     {
                         if (Items[i].Quality < 50)
-                        {
+                        {   //Aged Brie quality rises after experation
                             Items[i].Quality = Items[i].Quality + 1;
                         }
                     }

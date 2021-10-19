ff6fe976f1890974665815421152512e8046547d
jeroleski <74965501+jeroleski@users.noreply.github.com>
2021-10-19 11:42:33 +0200

    Implemented ConjuredTests, Relocated Inn and Item to seperate files for easier codeaccess in longer classes, Implemented helper-methods for special types of Items in Inn

--- GildedRose.Tests/TestSulfuras.cs
+++ GildedRose.Tests/TestSulfuras.cs
@@ -6,11 +6,66 @@ namespace GildedRose.Tests
 {
     public class TestSulfuras
     {
-        
+        [Fact]
+        public void Conjured_quality_drops_by_2_when_updating_1_time()
+        {
+            var inn = new Inn() {
+                Items = new List<Item> {
+                    new Item{Name = "Conjured Cheese Cake", SellIn = 4, Quality = 7}
+                }
+            };
 
-        
+            inn.UpdateQuality();
+
+            Assert.Equal(5, inn.Items[0].Quality);
+        }
+
+        [Fact]
+        public void Conjured_quality_drops_by_6_when_updating_3_times()
+        {
+            var inn = new Inn() {
+                Items = new List<Item> {
+                    new Item{Name = "Conjured Cheese Cake", SellIn = 4, Quality = 7}
+                }
+            };
+
+            inn.UpdateQuality();
+            inn.UpdateQuality();
+            inn.UpdateQuality();
 
 
+            Assert.Equal(1, inn.Items[0].Quality);
+        }
 
+        [Fact]
+        public void Conjured_quality_wont_drop_below_0()
+        {
+            var inn = new Inn() {
+                Items = new List<Item> {
+                    new Item{Name = "Conjured Cheese Cake", SellIn = 4, Quality = 7}
+                }
+            };
+
+            inn.UpdateQuality();
+            inn.UpdateQuality();
+            inn.UpdateQuality();
+            inn.UpdateQuality();
+
+            Assert.Equal(0, inn.Items[0].Quality);
+        }
+
+        [Fact]
+        public void Conjured_quality_drops_double_when_expired()
+        {
+            var inn = new Inn() {
+                Items = new List<Item> {
+                    new Item{Name = "Conjured Coka Cola", SellIn = 0, Quality = 20}
+                }
+            };
+
+            inn.UpdateQuality();
+
+            Assert.Equal(18, inn.Items[0].Quality);
+        }
     }
 }
